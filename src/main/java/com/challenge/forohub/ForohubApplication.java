caf7c872f58f141c11c1c91a9d1c8fb11b41582d
martin-b-07e5 <martin.bergagno@gmail.com>
2025-01-17 22:11:10 -0300

    first commit

---
+++ src/main/java/com/challenge/forohub/ForohubApplication.java
@@ -0,0 +1,14 @@
+package com.challenge.forohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForohubApplication.class, args);
+		System.out.println("\nAPI Server started...");
+	}
+
+}
